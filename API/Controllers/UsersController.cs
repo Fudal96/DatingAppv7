d6d629057a3abbd70684eeba53675569320bc729
Fudal96 <fudal22@gmail.com>
2023-03-09 13:59:31 +0100

    S2/16 Making our the Asynchronous

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,6 +3,7 @@
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -20,17 +21,17 @@ namespace API.Controllers
 
         // Api Endpoint so that we can request a list of users
         [HttpGet]
-        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-            var users = _context.Users.ToList();
+            var users = await _context.Users.ToListAsync();
 
             return users;
         }
         
         [HttpGet("{id}")]
-        public ActionResult<AppUser> GetUser(int id)
+        public async Task <ActionResult<AppUser>> GetUser(int id)
         {
-            return _context.Users.Find(id);
+            return await _context.Users.FindAsync(id);
 
         }
     }
