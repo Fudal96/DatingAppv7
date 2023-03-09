6e6fe50fd13c092d2dbf505b5b369d71b9bb13ad
Fudal96 <fudal22@gmail.com>
2023-03-09 21:16:58 +0100

    S4/39 - Adding a LOGIN endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -40,6 +40,26 @@ namespace API.Controllers
             return user;
          }
 
+         [HttpPost("login")]
+         public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+         {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+            // if the user does not exist in our database we can't let him log in
+            if (user == null) return Unauthorized("invalid username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("invalid password");
+            }
+
+            return user;
+         }
+
          private async Task<bool> UserExists(string username)
          {
             return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
