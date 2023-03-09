dfb1471cf5834d3244085e1a483087cfbc9a71f1
Fudal96 <fudal22@gmail.com>
2023-03-09 15:07:17 +0100

    S3/24 - Adding CORS in the API

--- API/Program.cs
+++ API/Program.cs
@@ -11,11 +11,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
     // We need to get a connection string from configuration
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline. (Order here is very very important!)
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
