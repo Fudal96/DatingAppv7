074ba7bb72da7f2336ba0740ecebf9b55e6fc2ff
Fudal96 <fudal22@gmail.com>
2023-03-10 11:44:28 +0100

    S5/51 - Injecting our AccountService into the nav component

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit{
-model: any = {}
+model: any = {};
+loggedIn = false;
 
 
-  constructor() {}
+constructor(private accountService: AccountService) {}
 
 
   ngOnInit(): void {
@@ -17,7 +19,13 @@ model: any = {}
   }
 
   login() {
-    console.log(this.model);
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.loggedIn = true;
+      },
+      error: error => console.log(error)
+    })
   }
 
 }
