5f366ee44ffa86c5a5cc5502b4302e65ab439335
Fudal96 <fudal22@gmail.com>
2023-03-10 13:11:59 +0100

    S5/55 - S5/55 - Persisting the login status in the LocalStorage using an Observable

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +12,25 @@ export class AppComponent implements OnInit {
   title = 'DatingAppv7';
   users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private accountService: AccountService) {}
 
   ngOnInit():void {
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  getUsers() {
     this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    })
+    next: response => this.users = response,
+    error: error => console.log(error),
+    complete: () => console.log('Request has completed')
+  })
+}
+
+  setCurrentUser() {
+    const userstring = localStorage.getItem('user');
+    if (!userstring) return;
+    const user: User = JSON.parse(userstring);
+    this.accountService.setCurrentUser(user);
   }
 }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -15,7 +15,15 @@ constructor(private accountService: AccountService) {}
 
 
   ngOnInit(): void {
+    this.getCurrentUser();
+  }
 
+  getCurrentUser() {
+    this.accountService.currentUser$.subscribe({
+      // the below method with !! turns it into a boolean and return true if we have a user and false if we don't have a user
+      next: user => this.loggedIn = !!user,
+      error: error => console.log(error)
+    })
   }
 
   login() {
@@ -29,6 +37,7 @@ constructor(private accountService: AccountService) {}
   }
 
   logout() {
+    this.accountService.logout();
     this.loggedIn = false;
   }
 
