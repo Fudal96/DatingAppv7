448f48ac1db59d4d45c3e14da71f0c8c271f684b
Fudal96 <fudal22@gmail.com>
2023-03-10 15:21:17 +0100

    S5/56 - Using the async pipe so that we automatically unsubscribe from a method

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -8,29 +10,21 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit{
 model: any = {};
-loggedIn = false;
 
 
-constructor(private accountService: AccountService) {}
+constructor(public accountService: AccountService) {}
 
 
   ngOnInit(): void {
-    this.getCurrentUser();
   }
 
-  getCurrentUser() {
-    this.accountService.currentUser$.subscribe({
-      // the below method with !! turns it into a boolean and return true if we have a user and false if we don't have a user
-      next: user => this.loggedIn = !!user,
-      error: error => console.log(error)
-    })
-  }
+
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: response => {
         console.log(response);
-        this.loggedIn = true;
+
       },
       error: error => console.log(error)
     })
@@ -38,7 +32,6 @@ constructor(private accountService: AccountService) {}
 
   logout() {
     this.accountService.logout();
-    this.loggedIn = false;
   }
 
 }
