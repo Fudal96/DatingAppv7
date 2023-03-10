b57c98d2e137d289f9d9128fb1fbffca35869e91
Fudal96 <fudal22@gmail.com>
2023-03-10 16:24:38 +0100

    S5/59 - Parent to child communication

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit{
-model: any = []
+  @Input() usersFromHomeComponent: any;
+model: any = {}
 
 
   constructor() {}
