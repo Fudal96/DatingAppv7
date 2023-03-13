9011845f4f2dd095132ab56a7d562facd9114f50
Fudal96 <fudal22@gmail.com>
2023-03-13 15:28:31 +0100

    S8/88 - Seeding data part one

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
+using API.Data;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,5 +25,19 @@ app.UseAuthentication(); // Do you have a valid token?
 app.UseAuthorization(); // Ok, You do have a valid token, What are you allowed to do then?
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during a migration");
+}
 
 app.Run();
