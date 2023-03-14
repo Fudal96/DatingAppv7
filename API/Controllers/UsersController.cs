3d8327da9c7928a738cde1fd37fb6b9b5722c310
Fudal96 <fudal22@gmail.com>
2023-03-14 09:02:10 +0100

    S8/95 - Using AutoMapper

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
 
+using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,9 +14,11 @@ namespace API.Controllers
         // Now we are changing it and instead of injecting DataContext we inject IUserRopository (it is kind of an abstration of an abstraction but that way it is easier to test our code)
 
         private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
         
-        public UsersController(IUserRepository userRepository)
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
+            _mapper = mapper;
             _userRepository = userRepository;
             
         }
@@ -23,16 +27,24 @@ namespace API.Controllers
         // Api Endpoint so that we can request a list of users
         
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            return Ok(await _userRepository.GetUsersAsync());
+            var users = await _userRepository.GetUsersAsync();
+
+            var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
+
+            return Ok(usersToReturn);
 
         }
         
         [HttpGet("{username}")]
-        public async Task <ActionResult<AppUser>> GetUser(string username)
+        public async Task <ActionResult<MemberDto>> GetUser(string username)
         {
-            return await _userRepository.GetUserByUsernnameAsync(username);
+            var user = await _userRepository.GetUserByUsernnameAsync(username);
+
+            var userToReturn = _mapper.Map<MemberDto>(user);
+
+            return Ok(userToReturn);
 
         }
     }
