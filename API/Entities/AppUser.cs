a31d6da82e55abd4f595ec537ba4b0744da9f1df
Fudal96 <fudal22@gmail.com>
2023-03-14 10:56:35 +0100

    S8/97 - Using AutoMapper queryable extensions to slighly improve the effeciency of the app

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -21,9 +21,9 @@ namespace API.Entities
         public List<Photo> Photos { get; set; } = new();
 
 
-        public int GetAge()
-        {
-            return DateOfBirth.CalculateAge();
-        }
+       // public int GetAge()
+        //{
+        //  return DateOfBirth.CalculateAge();
+       // }
     }
 }
