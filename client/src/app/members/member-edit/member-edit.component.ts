ab03a9f2b33f821de6766a84e1a0fa5f59e202ea
Fudal96 <fudal22@gmail.com>
2023-04-05 20:11:13 +0200

    S10/117 - Adding a Can Deactivate route guard , Preventing  leaving the site with unsaved changes

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { Member } from 'src/app/_models/member';
 import { User } from 'src/app/_models/user';
 import { MembersService } from 'src/app/_services/members.service';
@@ -14,6 +14,11 @@ import { NgForm } from '@angular/forms';
 })
 export class MemberEditComponent implements OnInit{
   @ViewChild('editForm') editForm: NgForm | undefined
+  @HostListener('window:beforeunload', ['$event']) unloadNotification($event:any) {
+    if (this.editForm?.dirty) {
+      $event.returnValue = true;
+    }
+  }
   member: Member | undefined;
   user: User | null = null;
 
