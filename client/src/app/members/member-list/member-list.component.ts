bef3ad534889b225b97fa76c30e8f31bdf4bbe45
Fudal96 <fudal22@gmail.com>
2023-04-06 07:39:54 +0200

    S10/121 - Using the service to store state (so that we don't call the api every time we want something when we already got it and can store it inside of a service so that it doesn't get destroyed)

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 
@@ -8,7 +9,7 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit{
-  members: Member[] = [];
+  members$: Observable<Member[]> | undefined;
 
 
 
@@ -16,17 +17,7 @@ export class MemberListComponent implements OnInit{
 
 
   ngOnInit(): void {
-    this.loadMembers();
-  }
-
-  loadMembers() {
-    this.memberService.getMembers().subscribe({
-      next: members => {
-        if (members) {
-          this.members = members
-        }
-      }
-    })
+    this.members$ = this.memberService.getMembers();
   }
 
 }
