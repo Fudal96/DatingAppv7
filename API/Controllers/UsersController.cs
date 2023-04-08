adc7315571be39e1e1ce6374e4d5988007ca4325
Fudal96 <fudal22@gmail.com>
2023-04-08 11:09:35 +0200

    S11/133 - Setting the main photo in the API

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -91,6 +91,31 @@ namespace API.Controllers
         return BadRequest("Problem adding photo");
     }
 
+    [HttpPut("set-main-photo/{photoId}")]
+    public async Task<ActionResult> SetMainPhoto(int photoId)
+    {
+        var user = await _userRepository.GetUserByUsernnameAsync(User.GetUsername());
+
+         if (user == null) return NotFound();
+
+         // we need to get hold of the photo
+         var photo = user.Photos.FirstOrDefault(x => x.Id == photoId);
+
+          if (photo == null) return NotFound();
+
+         if (photo.IsMain) return BadRequest("This is already your main photo");
+
+         var currentMain = user.Photos.FirstOrDefault(x => x.IsMain);
+
+         if (currentMain != null) currentMain.IsMain = false;
+
+         photo.IsMain = true;
+
+         if (await _userRepository.SaveAllAsync()) return NoContent();
+
+         return BadRequest("Problem setting the main photo");
+    }
+
 
     }
 
